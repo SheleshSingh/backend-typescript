1b564b298c7b5cda7d10a8f6bec7c505d60ef030
Shelesh-rdx <sheleshsingh755@gmail.com>
2025-06-04 12:45:17 +0530

    add users validation

--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,7 @@ import express, { Request, Response } from "express";
 import globalErrorHandler from "./middlewares/globalErrorHandler";
 import userRouter from "./user/userRouter";
 const app = express();
+app.use(express.json());
 app.get("/", (req: Request, res: Response) => {
   res.json({ message: "Hello World!" });
 });
--- src/user/userController.ts
+++ src/user/userController.ts
@@ -1,6 +1,12 @@
-import { Request, Response } from "express";
+import { NextFunction, Request, Response } from "express";
+import createHttpError from "http-errors";
 
-const createUser = (_req: Request, res: Response) => {
+const createUser = (req: Request, res: Response, next: NextFunction) => {
+  const { name, email, password } = req.body;
+  if (!name || !email || !password) {
+    const error = createHttpError(400, "All fields are required");
+    return next(error);
+  }
   res.json({ message: "User registered successfully!" });
 };
 export { createUser };
